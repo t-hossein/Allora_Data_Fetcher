cf0c3aeaa800697ca76ee618a1a70097a4c321cf
Hossein Teimouri <138848350+t-hossein@users.noreply.github.com>
2025-04-01 16:55:24 +0330

    example.py

--- example.py
+++ example.py
@@ -1,48 +1,9 @@
-import json
-from flask import Flask, Response
-#from config import app_base_path, model_file_path, download_path
-import os
-import json
-import pickle
-import pandas as pd
-import numpy as np
-from datetime import datetime
-from flask import Flask, jsonify, Response
-import torch
-import torch.nn as nn
-from torch.nn import functional as F
-import requests
-import traceback
-import logging
-import time
-import data
+from data import DataFetcher
 from datetime import datetime, timedelta
-from sklearn.preprocessing import StandardScaler
-import xgboost as xgb
-import joblib
-
-
-urls_oecd = {
-    "Growth Rate": "https://sdmx.oecd.org/public/rest/data/OECD.SDD.STES,DSD_KEI@DF_KEI,4.0/USA.M.PRVM.IX.BTE..?startPeriod=2020-01",
-    "Unemployment Rate": "https://sdmx.oecd.org/public/rest/data/OECD.SDD.TPS,DSD_LFS@DF_IALFS_UNE_M,1.0/USA..._Z.Y._T.Y_GE15..M?startPeriod=2008-01"
-}
-
-dic_stlouis = {
-    "CPI_1":"MEDCPIM158SFRBCLE",
-    "Dollar Index":'DTWEXBGS',
-    "Uncertainty":'VIXCLS',
-    "CPI_2":"CORESTICKM159SFRBATL"
-    }
-
-
-tickers = ['USCI', '^KS11', '000300.SS', "GC=F", "CL=F", "ES=F", "DX-Y.NYB", "EURUSD=X"]#
-
-keywords = ['ETH', "BTC"]
-
-
-granule = 5
-start_date = datetime.utcnow() - timedelta(days=4)
-end_date = datetime.utcnow()
-tiingo = "YOUR_TIINGO_API_KEY"
-dataobj = DataFetcher(start_date = start_date, end_date = end_date,symbol = "BTCUSD", granularity = f"{granule}min", volatility = False, tiingo=tiingo, new =True)
-train = dataobj.train_vol(symbol = "BTCUSD", urls_oecd= urls_oecd, dic_stlouis = dic_stlouis, keywords= keywords, tickers = tickers, train = False)
+start_date=datetime.utcnow() - timedelta(days=20)
+end_date=datetime.utcnow()
+TIINGO_API_KEY = "YOUR_API_TIINGO_KEY"
+#volatility_parameters = {"volatility_granularity": "5min", "standardized":True}
+data = DataFetcher(start_date = start_date, end_date = end_date ,symbol = "ETHUSD", granularity = "5min", TIINGO_API_KEY=TIINGO_API_KEY)
+ETH_Data = data.tiingo_crypto_data(prediction_horizon=360)
+ETH_Data
